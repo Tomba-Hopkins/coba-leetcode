a79ba85027144c3e2f58a8cca0dac4e936b30589
Tomba-Hopkins <saputrazamharimirza@gmail.com>
2024-02-24 10:05:25 +0700

    HUAGRGH SUSAH BANGET

--- main.go
+++ main.go
@@ -1,31 +1,7 @@
 package main
 
-// func isValid(s string) bool {
 
-// 	stack := []byte{}
-
-// 	kotak := map[byte]byte{
-// 		'(' : ')',
-// 		'{' : '}',
-// 		'[' : ']',
-// 	}
-
-// 	for i := 0; i < len(s); i++{
-// 		karakter := s[i]
-
-// 		if _, ok := kotak[karakter]; ok{
-// 			stack = append(stack, karakter)
-// 		} else {
-// 			if len(s) == 0 ||karakter != kotak[stack[len(stack) - 1]] {
-// 				return false
-// 			}
-// 			stack = stack[:len(stack) - 1]
-// 		}
-// 	}
-// 	return len(stack) == 0
-// }
 
 func main() {
-	// text := "(}"
-	// fmt.Println(isValid(text))
+	
 }
