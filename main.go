a1167c5d3eef26f41d5377f6e96dd27a25276936
Tomba-Hopkins <saputrazamharimirza@gmail.com>
2024-03-28 09:02:18 +0700

    hauuuchuchuch

--- main.go
+++ main.go
@@ -1,18 +1,7 @@
 package main
 
-import (
-	"fmt"
-	"math"
-)
 
-func squirt(num int) bool {
-    sqr := math.Sqrt(float64(num))
-	
-	exp := int(sqr)
-
-	return exp * exp == num
-}
 
 func main() {
-	fmt.Println(squirt(81))
+
 }
