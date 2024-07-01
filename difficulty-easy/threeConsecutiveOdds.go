ffa9b54f1906cbb5f4276e2abd02632ea32f37c4
Tomba-Hopkins <saputrazamharimirza@gmail.com>
2024-07-01 09:58:30 +0700

    sesbjbdjsd hjhhshddv hacuh

---
+++ difficulty-easy/threeConsecutiveOdds.go
@@ -0,0 +1,42 @@
+package difficultyeasy
+
+// Question :
+
+// Given an integer array arr, return true if there are three consecutive odd numbers in the array. Otherwise, return false.
+// Example 1:
+// Input: arr = [2,6,4,1]
+// Output: false
+// Explanation: There are no three consecutive odds.
+// Example 2:
+// Input: arr = [1,2,34,3,4,5,7,23,12]
+// Output: true
+// Explanation: [5,7,23] are three consecutive odds.
+
+// Answer :
+
+func ThreeConsecutiveOdds(arr []int) bool {
+
+	d := 0
+
+	streak := false
+	
+	for i := 0; i < len(arr); i++{
+		if arr[i] % 2 != 0{
+			streak = true
+		} else {
+			streak = false 
+			d = 0
+		}
+
+		if streak{
+			d++
+		}
+
+		if d == 3 {
+			return true
+		}
+	}
+	
+	return false
+	
+}
