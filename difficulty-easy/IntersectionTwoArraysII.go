d363da0a9c10854c910c3f3a37f8db9e8c9e8732
Tomba-Hopkins <saputrazamharimirza@gmail.com>
2024-07-02 08:29:54 +0700

    huhuaUGGRHRHg

---
+++ difficulty-easy/IntersectionTwoArraysII.go
@@ -0,0 +1,40 @@
+package difficultyeasy
+
+// Question :
+
+// Given two integer arrays nums1 and nums2, return an array of their intersection. Each element in the result must appear as many times as it shows in both arrays and you may return the result in any order.
+// Example 1:
+// Input: nums1 = [1,2,2,1], nums2 = [2,2]
+// Output: [2,2]
+// Example 2:
+// Input: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+// Output: [4,9]
+// Explanation: [9,4] is also accepted.
+// Constraints:
+// 1 <= nums1.length, nums2.length <= 1000
+// 0 <= nums1[i], nums2[i] <= 1000
+
+// Answer :
+func IntersectionTwoArraysII(nums1 []int, nums2 []int) (res []int) {
+
+	box1 := map[int]int{}
+	box2 := map[int]int{}
+
+	for _, n1 := range nums1 {
+		box1[n1]++
+	}
+
+	for _, n2 := range nums2 {
+		box2[n2]++
+	}
+
+	for _, r := range nums1 {
+		if box1[r] > 0 && box2[r] > 0 {
+			res = append(res, r)
+			box1[r]--
+			box2[r]--
+		}
+	}
+
+	return
+}
