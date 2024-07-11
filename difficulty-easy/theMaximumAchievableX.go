1e7a0e8f4eb509d6e26e8c6c96d602fbf71d6278
Tomba-Hopkins <saputrazamharimirza@gmail.com>
2024-07-11 07:39:12 +0700

    hacccuhhh

---
+++ difficulty-easy/theMaximumAchievableX.go
@@ -0,0 +1,34 @@
+package difficultyeasy
+
+// Question :
+
+// Given two integers, num and t. A number is achievable if it can become equal to num after applying the following operation:
+// Increase or decrease the number by 1, and simultaneously increase or decrease num by 1.
+// Return the maximum achievable number after applying the operation at most t times.
+// Example 1:
+// Input: num = 4, t = 1
+// Output: 6
+// Explanation:
+// Apply the following operation once to make the maximum achievable number equal to num:
+// Decrease the maximum achievable number by 1, and increase num by 1.
+// Example 2:
+// Input: num = 3, t = 2
+// Output: 7
+// Explanation:
+// Apply the following operation twice to make the maximum achievable number equal to num:
+// Decrease the maximum achievable number by 1, and increase num by 1.
+// Constraints:
+// 1 <= num, t <= 50
+
+// Answer :
+
+func TheMaximumAchievableX(num int, t int) int {
+
+	for i := 0; i < t; i++{
+		num++
+		num++
+	}
+
+	return num
+
+}
