d2f56024716cbd4e32b078ca1e4e108729c6e01c
Tomba-Hopkins <saputrazamharimirza@gmail.com>
2024-08-12 06:19:03 +0700

    komat kamit baca mantra

---
+++ difficulty-easy/halvesAreAlike.go
@@ -0,0 +1,45 @@
+package difficultyeasy
+
+// Question :
+
+// You are given a string s of even length. Split this string into two halves of equal lengths, and let a be the first half and b be the second half.
+// Two strings are alike if they have the same number of vowels ('a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'). Notice that s contains uppercase and lowercase letters.
+// Return true if a and b are alike. Otherwise, return false.
+// Example 1:
+// Input: s = "book"
+// Output: true
+// Explanation: a = "bo" and b = "ok". a has 1 vowel and b has 1 vowel. Therefore, they are alike.
+// Example 2:
+// Input: s = "textbook"
+// Output: false
+// Explanation: a = "text" and b = "book". a has 1 vowel whereas b has 2. Therefore, they are not alike.
+// Notice that the vowel o is counted twice.
+// Constraints:
+// 2 <= s.length <= 1000
+// s.length is even.
+// s consists of uppercase and lowercase letters.
+
+// Answer :
+func HalvesAreAlike(s string) bool {
+
+	x, y := 0, 0
+	v := "aiueoAIUEO"
+	m := map[string]bool{}
+
+	for _, r := range v {
+		m[string(r)] = true
+	}
+	for i := 0; i < len(s); i++{
+		if i < len(s) / 2 {
+			if m[string(s[i])]{
+				x++
+			}
+		} else if i >= len(s) / 2 {
+			if m[string(s[i])] {
+				y++
+			}
+		}
+		}
+	return y == x
+
+}
