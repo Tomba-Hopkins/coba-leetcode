b96425e05d65e63a53887a7a9768438860b9b8ea
Tomba-Hopkins <saputrazamharimirza@gmail.com>
2024-08-29 06:48:45 +0700

    add: new quest

---
+++ difficulty-easy/numJewelsInStones.go
@@ -0,0 +1,31 @@
+package difficultyeasy
+
+// Question :
+
+// You're given strings jewels representing the types of stones that are jewels, and stones representing the stones you have. Each character in stones is a type of stone you have. You want to know how many of the stones you have are also jewels.
+// Letters are case sensitive, so "a" is considered a different type of stone from "A".
+// Example 1:
+// Input: jewels = "aA", stones = "aAAbbbb"
+// Output: 3
+// Example 2:
+// Input: jewels = "z", stones = "ZZ"
+// Output: 0
+// Constraints:
+// 1 <= jewels.length, stones.length <= 50
+// jewels and stones consist of only English letters.
+// All the characters of jewels are unique.
+
+// Answer :
+
+func NumJewelsInStones(jewels string, stones string) (r int) {
+	m := map[rune]bool{}
+	for _, j := range jewels {
+		m[j] = true
+	}
+	for _, s := range stones {
+		if m[s] {
+			r++
+		}
+	}
+	return
+}
