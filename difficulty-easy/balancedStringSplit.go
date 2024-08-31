81be5a8ba6afcb994f3cfdca1f5be6c7114b8b6a
Tomba-Hopkins <saputrazamharimirza@gmail.com>
2024-08-31 07:26:53 +0700

    hshshsh

---
+++ difficulty-easy/balancedStringSplit.go
@@ -0,0 +1,48 @@
+package difficultyeasy
+
+// Question :
+
+// Balanced strings are those that have an equal quantity of 'L' and 'R' characters.
+// Given a balanced string s, split it into some number of substrings such that:
+// Each substring is balanced.
+// Return the maximum number of balanced strings you can obtain.
+// Example 1:
+// Input: s = "RLRRLLRLRL"
+// Output: 4
+// Explanation: s can be split into "RL", "RRLL", "RL", "RL", each substring contains same number of 'L' and 'R'.
+// Example 2:
+// Input: s = "RLRRRLLRLL"
+// Output: 2
+// Explanation: s can be split into "RL", "RRRLLRLL", each substring contains same number of 'L' and 'R'.
+// Note that s cannot be split into "RL", "RR", "RL", "LR", "LL", because the 2nd and 5th substrings are not balanced.
+// Example 3:
+// Input: s = "LLLLRRRR"
+// Output: 1
+// Explanation: s can be split into "LLLLRRRR".
+// Constraints:
+// 2 <= s.length <= 1000
+// s[i] is either 'L' or 'R'.
+// s is a balanced string.
+
+// Answer :
+func BalancedStringSplit(s string) (r int) {
+	d := ""
+	x := 0
+	for _, v:= range s {
+		if string(v) == d {
+			x++
+		}
+		if d == ""{
+			d = string(v)
+			x++
+		}
+		if x > 0 && string(v) != d{
+			x--
+		}
+		if x == 0 {
+			r++
+			d = ""
+		}
+	}
+	return
+}
