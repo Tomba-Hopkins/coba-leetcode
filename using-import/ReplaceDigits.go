d5964cfee4d09fb3bc08ac819c2f0ce6a8384f5e
Tomba-Hopkins <saputrazamharimirza@gmail.com>
2024-10-10 08:51:16 +0700

    add: replace the digits

---
+++ using-import/ReplaceDigits.go
@@ -0,0 +1,45 @@
+package usingimportpackage
+
+import "strconv"
+
+// Question :
+
+// You are given a 0-indexed string s that has lowercase English letters in its even indices and digits in its odd indices.
+// You must perform an operation shift(c, x), where c is a character and x is a digit, that returns the xth character after c.
+// For example, shift('a', 5) = 'f' and shift('x', 0) = 'x'.
+// For every odd index i, you want to replace the digit s[i] with the result of the shift(s[i-1], s[i]) operation.
+// Return s after replacing all digits. It is guaranteed that shift(s[i-1], s[i]) will never exceed 'z'.
+// Note that shift(c, x) is not a preloaded function, but an operation to be implemented as part of the solution.
+// Example 1:
+// Input: s = "a1c1e1"
+// Output: "abcdef"
+// Explanation: The digits are replaced as follows:
+// - s[1] -> shift('a',1) = 'b'
+// - s[3] -> shift('c',1) = 'd'
+// - s[5] -> shift('e',1) = 'f'
+// Example 2:
+// Input: s = "a1b2c3d4e"
+// Output: "abbdcfdhe"
+// Explanation: The digits are replaced as follows:
+// - s[1] -> shift('a',1) = 'b'
+// - s[3] -> shift('b',2) = 'd'
+// - s[5] -> shift('c',3) = 'f'
+// - s[7] -> shift('d',4) = 'h'
+// Constraints:
+// 1 <= s.length <= 100
+// s consists only of lowercase English letters and digits.
+// shift(s[i-1], s[i]) <= 'z' for all odd indices i.
+
+// Answer :
+func ReplaceDigits(s string) (r string) {
+    for i := 0; i < len(s); i++{
+        if i % 2 != 0 {
+            n, _ := strconv.Atoi(string(s[i]))
+            char := rune(s[i - 1])
+            r += string(char + rune(n))
+        } else {
+            r += string(s[i])
+        }
+    }
+    return 
+}
