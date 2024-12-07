cf659a23b4853d3afc25f39d52a2887e10105ea4
Tomba-Hopkins <saputrazamharimirza@gmail.com>
2024-12-07 08:52:53 +0700

    add: CountTheNumberOfVoewlStringsInRange

---
+++ difficulty-easy/CountTheNumberOfVoewlStringsInRange.go
@@ -0,0 +1,48 @@
+package difficultyeasy
+
+// Question :
+
+// You are given a 0-indexed array of string words and two integers left and right.
+// A string is called a vowel string if it starts with a vowel character and ends with a vowel character where vowel characters are 'a', 'e', 'i', 'o', and 'u'.
+// Return the number of vowel strings words[i] where i belongs to the inclusive range [left, right].
+// Example 1:
+// Input: words = ["are","amy","u"], left = 0, right = 2
+// Output: 2
+// Explanation:
+// - "are" is a vowel string because it starts with 'a' and ends with 'e'.
+// - "amy" is not a vowel string because it does not end with a vowel.
+// - "u" is a vowel string because it starts with 'u' and ends with 'u'.
+// The number of vowel strings in the mentioned range is 2.
+// Example 2:
+// Input: words = ["hey","aeo","mu","ooo","artro"], left = 1, right = 4
+// Output: 3
+// Explanation:
+// - "aeo" is a vowel string because it starts with 'a' and ends with 'o'.
+// - "mu" is not a vowel string because it does not start with a vowel.
+// - "ooo" is a vowel string because it starts with 'o' and ends with 'o'.
+// - "artro" is a vowel string because it starts with 'a' and ends with 'o'.
+// The number of vowel strings in the mentioned range is 3.
+// Constraints:
+// 1 <= words.length <= 1000
+// 1 <= words[i].length <= 10
+// words[i] consists of only lowercase English letters.
+// 0 <= left <= right < words.length
+
+// Answer :
+func CountTheNumberOfVoewlStringsInRange(words []string, left int, right int) (r int) {
+    vowel := "aiueo"
+    m := map[string]bool{}
+    for _, v := range vowel {
+        m[string(v)] = true
+    }
+    for i := left; i <= right; i++{
+        if  m[string(words[i])] {
+            r++
+            continue
+        }
+        if m[string(words[i][0])] && m[string(words[i][len(words[i]) - 1])] {
+            r++
+        }
+    }
+    return
+}
