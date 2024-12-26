3dfbdf40d4e39b328abd7097432e482fa2804b76
Tomba-Hopkins <saputrazamharimirza@gmail.com>
2024-12-26 08:32:55 +0700

    add: SortVowelsInAString

--- main.go
+++ main.go
@@ -1,16 +1,5 @@
 package main
 
-func ContainsDuplicate(nums []int) bool {
-    m := map[int]bool{}
-    for _, n := range nums {
-        if m[n] {
-            return true
-        }
-        m[n] = true
-    }
-	return false
-}
-
 func main() {
 
 }
