c00a7831ce6ccb8955169be5ca2854f638c8b952
Tomba-Hopkins <saputrazamharimirza@gmail.com>
2025-01-18 19:13:06 +0700

    add: CheckIfNumberHasEqDigitCountAndDigitVal

---
+++ using-import/CheckIfNumberHasEqDigitCountAndDigitVal.go
@@ -0,0 +1,45 @@
+package usingimportpackage
+
+import "strconv"
+
+// Question :
+
+// You are given a 0-indexed string num of length n consisting of digits.
+// Return true if for every index i in the range 0 <= i < n, the digit i occurs num[i] times in num, otherwise return false.
+// Example 1:
+// Input: num = "1210"
+// Output: true
+// Explanation:
+// num[0] = '1'. The digit 0 occurs once in num.
+// num[1] = '2'. The digit 1 occurs twice in num.
+// num[2] = '1'. The digit 2 occurs once in num.
+// num[3] = '0'. The digit 3 occurs zero times in num.
+// The condition holds true for every index in "1210", so return true.
+// Example 2:
+// Input: num = "030"
+// Output: false
+// Explanation:
+// num[0] = '0'. The digit 0 should occur zero times, but actually occurs twice in num.
+// num[1] = '3'. The digit 1 should occur three times, but actually occurs zero times in num.
+// num[2] = '0'. The digit 2 occurs zero times in num.
+// The indices 0 and 1 both violate the condition, so return false.
+// Constraints:
+// n == num.length
+// 1 <= n <= 10
+// num consists of digits.
+
+// Answer :
+func CheckIfNumberHasEqDigitCountAndDigitVal(num string) bool {
+    m := map[string]int{}
+    for _, x := range num {
+        m[string(x)]++
+    }
+    for i := 0; i < len(num); i++{
+        s := strconv.Itoa(i)
+        n, _ := strconv.Atoi(string(num[i]))
+        if m[s] != n {
+            return false
+        }
+    }
+    return true
+}
