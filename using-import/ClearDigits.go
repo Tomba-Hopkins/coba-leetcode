7228b5be501b52cc09899bc54a995f489e9bdbbe
Tomba-Hopkins <saputrazamharimirza@gmail.com>
2025-02-10 07:51:15 +0700

    add: ClearDigits

---
+++ using-import/ClearDigits.go
@@ -0,0 +1,43 @@
+package usingimportpackage
+
+import "strings"
+
+// Question :
+
+// You are given a string s.
+// Your task is to remove all digits by doing this operation repeatedly:
+// Delete the first digit and the closest non-digit character to its left.
+// Return the resulting string after removing all digits.
+// Example 1:
+// Input: s = "abc"
+// Output: "abc"
+// Explanation:
+// There is no digit in the string.
+// Example 2:
+// Input: s = "cb34"
+// Output: ""
+// Explanation:
+// First, we apply the operation on s[2], and s becomes "c4".
+// Then we apply the operation on s[1], and s becomes "".
+// Constraints:
+// 1 <= s.length <= 100
+// s consists only of lowercase English letters and digits.
+// The input is generated such that it is possible to delete all digits.
+
+// Answer :
+func ClearDigits(s string) string {
+    d := map[string]bool{}
+    w := "1234567890"
+    for _, x := range w {
+        d[string(x)] = true
+    }
+    ngestuk := []string{}
+    for i := 0; i < len(s); i++{
+        if d[string(s[i])] {
+            ngestuk = ngestuk[: len(ngestuk) - 1]
+        } else {
+            ngestuk = append(ngestuk, string(s[i]))
+        }
+    }
+	return strings.Join(ngestuk, "")
+}
