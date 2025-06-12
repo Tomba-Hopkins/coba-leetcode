721e063ad46225da3100bcd670f267de4de6f686
Tomba-Hopkins <saputrazamharimirza@gmail.com>
2025-06-12 09:22:07 +0700

    add: MaxDiffBetweenAdjacentElInCircularArr

---
+++ difficulty-easy/MaxDiffBetweenAdjacentElInCircularArr.go
@@ -0,0 +1,45 @@
+package difficultyeasy
+
+// Question :
+
+// Given a circular array nums, find the maximum absolute difference between adjacent elements.
+// Note: In a circular array, the first and last elements are adjacent.
+// Example 1:
+// Input: nums = [1,2,4]
+// Output: 3
+// Explanation:
+// Because nums is circular, nums[0] and nums[2] are adjacent. They have the maximum absolute difference of |4 - 1| = 3.
+// Example 2:
+// Input: nums = [-5,-10,-5]
+// Output: 5
+// Explanation:
+// The adjacent elements nums[0] and nums[1] have the maximum absolute difference of |-5 - (-10)| = 5.
+// Constraints:
+// 2 <= nums.length <= 100
+// -100 <= nums[i] <= 100
+
+// Answer :
+func MaxDiffBetweenAdjacentElInCircularArr(nums []int) int {
+	r := 0
+	for i := 0; i < len(nums); i++ {
+		if i == len(nums)-1 {
+			x := nums[0] - nums[len(nums)-1]
+			if abs(x) > r {
+				r = abs(x)
+			}
+			break
+		}
+		x := nums[i] - nums[i+1]
+		if abs(x) > r {
+			r = abs(x)
+		}
+	}
+	return r
+}
+
+func abs(n int) int {
+	if n > 0 {
+		return n
+	}
+	return -n
+}
