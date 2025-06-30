ec4ca3c703ab6e820673e939869894d390762717
Tomba-Hopkins <saputrazamharimirza@gmail.com>
2025-06-30 08:49:39 +0700

    add: LongestHarmoniousSubseq

---
+++ difficulty-easy/LongestHarmoniousSubseq.go
@@ -0,0 +1,42 @@
+package difficultyeasy
+
+// Question :
+
+// We define a harmonious array as an array where the difference between its maximum value and its minimum value is exactly 1.
+// Given an integer array nums, return the length of its longest harmonious subsequence among all its possible subsequences.
+// Example 1:
+// Input: nums = [1,3,2,2,5,2,3,7]
+// Output: 5
+// Explanation:
+// The longest harmonious subsequence is [3,2,2,2,3].
+// Example 2:
+// Input: nums = [1,2,3,4]
+// Output: 2
+// Explanation:
+// The longest harmonious subsequences are [1,2], [2,3], and [3,4], all of which have a length of 2.
+// Example 3:
+// Input: nums = [1,1,1,1]
+// Output: 0
+// Explanation:
+// No harmonic subsequence exists.
+// Constraints:
+// 1 <= nums.length <= 2 * 104
+// -109 <= nums[i] <= 109
+
+// Answer :
+func LongestHarmoniousSubseq(nums []int) (r int) {
+	m := map[int]int{}
+	for _, n := range nums {
+		m[n]++
+	}
+	for k, v := range m {
+		n := k + 1
+		if m[n] == 0 {
+			continue
+		}
+		if v+m[n] > r {
+			r = v + m[n]
+		}
+	}
+	return
+}
