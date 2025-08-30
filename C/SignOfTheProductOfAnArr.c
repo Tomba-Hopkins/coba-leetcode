d5a03fed7634103a3ca69cfad7b835dc66b66308
Tomba-Hopkins <saputrazamharimirza@gmail.com>
2025-08-30 11:42:37 +0700

    add: SignOfTheProductOfAnArr

---
+++ C/SignOfTheProductOfAnArr.c
@@ -0,0 +1,33 @@
+// desc :
+
+// Implement a function signFunc(x) that returns:
+// 1 if x is positive.
+// -1 if x is negative.
+// 0 if x is equal to 0.
+// You are given an integer array nums. Let product be the product of all values in the array nums.
+// Return signFunc(product).
+// Example 1:
+// Input: nums = [-1,-2,-3,-4,3,2,1]
+// Output: 1
+// Explanation: The product of all values in the array is 144, and signFunc(144) = 1
+// Example 2:
+// Input: nums = [1,5,0,2,-3]
+// Output: 0
+// Explanation: The product of all values in the array is 0, and signFunc(0) = 0
+// Example 3:
+// Input: nums = [-1,1,-1,1,-1]
+// Output: -1
+// Explanation: The product of all values in the array is -1, and signFunc(-1) = -1
+// Constraints:
+// 1 <= nums.length <= 1000
+// -100 <= nums[i] <= 100
+
+// answer :
+int SignOfTheProductOfAnArr(int* nums, int numsSize) {
+    int countNeg = 0;
+    for (int i = 0; i < numsSize; i++) {
+        if (nums[i] == 0) return 0;
+        if (nums[i] < 0) countNeg++;
+    }
+    return (countNeg % 2 == 0) ? 1 : -1;
+}
