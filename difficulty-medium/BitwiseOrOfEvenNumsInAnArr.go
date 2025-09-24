e061e7dc77be7bcea849d64dee977fa2e1952e39
Tomba-Hopkins <saputrazamharimirza@gmail.com>
2025-09-24 09:31:50 +0700

    add: BitwiseOrOfEvenNumsInAnArr

---
+++ difficulty-medium/BitwiseOrOfEvenNumsInAnArr.go
@@ -0,0 +1,43 @@
+package difficultymedium
+
+// Question :
+
+// You are given an integer array nums.
+// Return the bitwise OR of all even numbers in the array.
+// If there are no even numbers in nums, return 0.
+// Example 1:
+// Input: nums = [1,2,3,4,5,6]
+// Output: 6
+// Explanation:
+// The even numbers are 2, 4, and 6. Their bitwise OR equals 6.
+// Example 2:
+// Input: nums = [7,9,11]
+// Output: 0
+// Explanation:
+// There are no even numbers, so the result is 0.
+// Example 3:
+// Input: nums = [1,8,16]
+// Output: 24
+// Explanation:
+// The even numbers are 8 and 16. Their bitwise OR equals 24.
+// Constraints:
+// 1 <= nums.length <= 100
+// 1 <= nums[i] <= 100
+
+// Answer :
+// bitwise or tuh kek gini
+// 8: 01000
+//16: 10000
+//out: 11000 -> 24
+func BitwiseOrOfEvenNumsInAnArr(nums []int) int {
+	x := 0
+	for i := 0; i < len(nums); i++ {
+		if x == 0 && nums[i]%2 == 0 {
+			x = nums[i]
+		} else if nums[i]%2 == 0 {
+			x |= nums[i]
+		}
+	}
+	return x
+}
+
