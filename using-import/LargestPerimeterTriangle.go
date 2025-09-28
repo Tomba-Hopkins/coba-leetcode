bb02e34cd1ad60cbaca6823403c1969a8d420bb0
Tomba-Hopkins <saputrazamharimirza@gmail.com>
2025-09-28 10:22:21 +0700

    add: LargestPerimeterTriangle

---
+++ using-import/LargestPerimeterTriangle.go
@@ -0,0 +1,36 @@
+package usingimportpackage
+
+import "sort"
+
+// Question :
+
+// Given an integer array nums, return the largest perimeter of a triangle with a non-zero area, formed from three of these lengths. If it is impossible to form any triangle of a non-zero area, return 0.
+// Example 1:
+// Input: nums = [2,1,2]
+// Output: 5
+// Explanation: You can form a triangle with three side lengths: 1, 2, and 2.
+// Example 2:
+// Input: nums = [1,2,1,10]
+// Output: 0
+// Explanation:
+// You cannot use the side lengths 1, 1, and 2 to form a triangle.
+// You cannot use the side lengths 1, 1, and 10 to form a triangle.
+// You cannot use the side lengths 1, 2, and 10 to form a triangle.
+// As we cannot use any three side lengths to form a triangle of non-zero area, we return 0.
+// Constraints:
+// 3 <= nums.length <= 104
+// 1 <= nums[i] <= 106
+
+// Answer :
+func LargestPerimeterTriangle(nums []int) (r int) {
+	sort.Ints(nums)
+	for i := 0; i < len(nums) - 2; i++{
+		x, y := nums[i], nums[i + 1]
+		for j := i + 2; j < len(nums); j++{
+			if x + y > nums[j] && x + y + nums[j] > r {
+				r = x + y + nums[j]
+			}
+		}	
+	}
+	return 
+}
