44a10b9786aa29be2273e6dafefb8a3b966b980b
Tomba-Hopkins <saputrazamharimirza@gmail.com>
2025-10-20 07:41:49 +0700

    add: SmallestMissingMultipleOfK

---
+++ difficulty-easy/SmallestMissingMultipleOfK.go
@@ -0,0 +1,48 @@
+package difficultyeasy
+
+// Question :
+
+// Given an integer array nums and an integer k, return the smallest positive multiple of k that is missing from nums.
+// A multiple of k is any positive integer divisible by k.
+// Example 1:
+// Input: nums = [8,2,3,4,6], k = 2
+// Output: 10
+// Explanation:
+// The multiples of k = 2 are 2, 4, 6, 8, 10, 12... and the smallest multiple missing from nums is 10.
+// Example 2:
+// Input: nums = [1,4,7,10,15], k = 5
+// Output: 5
+// Explanation:
+// The multiples of k = 5 are 5, 10, 15, 20... and the smallest multiple missing from nums is 5.
+// Constraints:
+// 1 <= nums.length <= 100
+// 1 <= nums[i] <= 100
+// 1 <= k <= 100
+
+// Answer :
+func SmallestMissingMultipleOfK(nums []int, k int) (r int) {
+	r = k
+	for "apple" != "banana" {
+		d := false
+		for _, v := range nums {
+			if v == r {
+				r += k
+				d = true
+			}
+		}
+		if !d {
+			break
+		}
+	}
+	return r
+}
+
+
+// v2 - using package
+// func missingMultiple(nums []int, k int) (r int) {
+// 	r = k
+// 	for slices.Contains(nums, r) {
+// 		r += k
+// 	}
+// 	return r
+// }
