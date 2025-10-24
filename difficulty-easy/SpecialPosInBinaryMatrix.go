a18ca1c567ca00c53c8a4067ca6a8b780663ee70
Tomba-Hopkins <saputrazamharimirza@gmail.com>
2025-10-24 09:46:40 +0700

    add: SpecialPosInBinaryMatrix

---
+++ difficulty-easy/SpecialPosInBinaryMatrix.go
@@ -0,0 +1,46 @@
+package difficultyeasy
+
+// Question :
+
+// Given an m x n binary matrix mat, return the number of special positions in mat.
+// A position (i, j) is called special if mat[i][j] == 1 and all other elements in row i and column j are 0 (rows and columns are 0-indexed).
+// Example 1:
+// Input: mat = [[1,0,0],[0,0,1],[1,0,0]]
+// Output: 1
+// Explanation: (1, 2) is a special position because mat[1][2] == 1 and all other elements in row 1 and column 2 are 0.
+// Example 2:
+// Input: mat = [[1,0,0],[0,1,0],[0,0,1]]
+// Output: 3
+// Explanation: (0, 0), (1, 1) and (2, 2) are special positions.
+// Constraints:
+// m == mat.length
+// n == mat[i].length
+// 1 <= m, n <= 100
+// mat[i][j] is either 0 or 1.
+
+// Answer :
+func SpecialPosInBinaryMatrix(mat [][]int) (r int) {
+	for i := 0; i < len(mat); i++ {
+		d, d2 := 0, 0
+		for j := 0; j < len(mat[i]); j++ {
+			t := 0
+			for k := 0; k < len(mat); k++ {
+				if mat[k][j] == 1 {
+					t++
+				}
+			}
+			if t == 1 && mat[i][j] == 1 {
+				d++
+			}
+		}
+		for _, v := range mat[i] {
+			if v == 1 {
+				d2++
+			}
+		}
+		if d == 1 && d2 == 1 {
+			r++
+		}
+	}
+	return
+}
