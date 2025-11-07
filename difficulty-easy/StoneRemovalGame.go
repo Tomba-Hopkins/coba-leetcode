21390a360a2637126b0fa3364f1b2a59fac1457b
Tomba-Hopkins <saputrazamharimirza@gmail.com>
2025-11-07 08:58:16 +0700

    add: StoneRemovalGame

---
+++ difficulty-easy/StoneRemovalGame.go
@@ -0,0 +1,39 @@
+package difficultyeasy
+
+// Question :
+
+// Alice and Bob are playing a game where they take turns removing stones from a pile, with Alice going first.
+// Alice starts by removing exactly 10 stones on her first turn.
+// For each subsequent turn, each player removes exactly 1 fewer stone than the previous opponent.
+// The player who cannot make a move loses the game.
+// Given a positive integer n, return true if Alice wins the game and false otherwise.
+// Example 1:
+// Input: n = 12
+// Output: true
+// Explanation:
+// Alice removes 10 stones on her first turn, leaving 2 stones for Bob.
+// Bob cannot remove 9 stones, so Alice wins.
+// Example 2:
+// Input: n = 1
+// Output: false
+// Explanation:
+// Alice cannot remove 10 stones, so Alice loses.
+// Constraints:
+// 1 <= n <= 50
+
+// Answer :
+func StoneRemovalGame(n int) bool {
+	d, s := false, 10
+	for n > 0 {
+		if !d && n < s {
+			return false
+		}
+		if n < s {
+			break
+		}
+		d = !d
+		n -= s
+		s--
+	}
+	return d
+}
