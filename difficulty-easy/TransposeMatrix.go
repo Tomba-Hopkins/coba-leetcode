19b83d739837ca70157cbbc8df2d1878e10dd9a1
Tomba-Hopkins <saputrazamharimirza@gmail.com>
2026-02-11 15:20:02 +0700

    add: TransposeMatrix

---
+++ difficulty-easy/TransposeMatrix.go
@@ -0,0 +1,30 @@
+package difficultyeasy
+
+// Question :
+
+// Given a 2D integer array matrix, return the transpose of matrix.
+// The transpose of a matrix is the matrix flipped over its main diagonal, switching the matrix's row and column indices.
+// Example 1:
+// Input: matrix = [[1,2,3],[4,5,6],[7,8,9]]
+// Output: [[1,4,7],[2,5,8],[3,6,9]]
+// Example 2:
+// Input: matrix = [[1,2,3],[4,5,6]]
+// Output: [[1,4],[2,5],[3,6]]
+// Constraints:
+// m == matrix.length
+// n == matrix[i].length
+// 1 <= m, n <= 1000
+// 1 <= m * n <= 105
+// -109 <= matrix[i][j] <= 109
+
+// Answer :
+func TransposeMatrix(matrix [][]int) (r [][]int) {
+	for i := 0; i < len(matrix[0]); i++ {
+		arr := []int{}
+		for j := 0; j < len(matrix); j++ {
+			arr = append(arr, matrix[j][i])
+		}
+		r = append(r, arr)
+	}
+	return
+}
