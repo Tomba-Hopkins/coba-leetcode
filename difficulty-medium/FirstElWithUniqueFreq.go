d5cf7baed4af62d8651adedeedebf28e31e2af30
Tomba-Hopkins <saputrazamharimirza@gmail.com>
2026-02-18 08:15:03 +0700

    add: CheckIfItIsAnStraightLine

---
+++ difficulty-medium/FirstElWithUniqueFreq.go
@@ -0,0 +1,57 @@
+package difficultymedium
+
+// Question :
+
+// You are given an integer array nums.
+// Return an integer denoting the first element (scanning from left to right) in nums whose frequency is unique. That is, no other integer appears the same number of times in nums. If there is no such element, return -1.
+// Example 1:
+// Input: nums = [20,10,30,30]
+// Output: 30
+// Explanation:
+// 20 appears once.
+// 10 appears once.
+// 30 appears twice.
+// The frequency of 30 is unique because no other integer appears exactly twice.
+// Example 2:
+// Input: nums = [20,20,10,30,30,30]
+// Output: 20
+// Explanation:
+// 20 appears twice.
+// 10 appears once.
+// 30 appears 3 times.
+// The frequency of 20, 10, and 30 are unique. The first element that has unique frequency is 20.
+// Example 3:
+// Input: nums = [10,10,20,20]
+// Output: -1
+// Explanation:
+// 10 appears twice.
+// 20 appears twice.
+// No element has a unique frequency.
+// Constraints:
+// 1 <= nums.length <= 105
+// 1 <= nums[i] <= 105
+
+// Answer :
+
+func FirstElWithUniqueFreq(nums []int) int {
+	m, m2, b := map[int]int{}, map[int]int{}, map[int]bool{}
+	for _, n := range nums {
+		m[n]++
+	}
+	nSingle := []int{}
+	for _, v := range nums {
+		if !b[v] {
+			nSingle = append(nSingle, v)
+			b[v] = true
+		}
+	}
+	for _, n := range nSingle {
+		m2[m[n]]++
+	}
+	for _, n := range nums {
+		if m2[m[n]] == 1 {
+			return n
+		}
+	}
+	return -1
+}
